b065ca7abac379aebfd369507fb8650e36bc921d
Dominik Plewa <plewad@gmail.com>
2018-01-03 16:31:15 +0100

    create fresh web app from start.spring.io

---
+++ src/main/java/pl/dplewa/awscidemo/AwsCiDemoApplication.java
@@ -0,0 +1,12 @@
+package pl.dplewa.awscidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCiDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/dplewa/awscidemo/AwsCiDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.dplewa.awscidemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AwsCiDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
